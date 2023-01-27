44b3b4e3bfd22b966567686301b3964349e2aeab
Zoltán Gombos <zoltan.gombos@sagemcom.hu>
2023-01-27 11:13:30 +0100

    Add project files.

---
+++ ExpanderTest/ExpanderTest/MainPageVM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ExpanderTest
+{
+    public class MainPageVM
+    {
+        public ObservableCollection<string> Strings { get; set; }
+
+        public MainPageVM()
+        {
+            Strings = new ObservableCollection<string>
+            {
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Six",
+                "Seven",
+                "Eight",
+            };
+        }
+    }
+}
